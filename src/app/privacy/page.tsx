c2b1e778b1fa83d493f557229a1d0779f3b865e7
maurice <mauricelandersiii@gmail.com>
2024-04-15 16:24:23 -0400

    add privacy page

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,53 @@
+export default function PrivacyPage() {
+  return (
+    <main className="flex flex-col items-center bg-background p-8">
+      <div className="flex min-h-screen max-w-5xl flex-col gap-3 text-lg text-text">
+        <h1 className="text-bold self-center text-3xl">Privacy Policy</h1>
+        <p className="text-center text-subtext">
+          Last Updated: April 14, 2024{" "}
+        </p>
+        <h2 className="font-medium">Introduction</h2>
+        <p>
+          Welcome to Who Was At. We respect your privacy and are committed to
+          protecting your personal data. This privacy policy will inform you
+          about how we look after your personal data when you visit our website
+          (regardless of where you visit from) and tell you about your privacy
+          rights and how the law protects you.
+        </p>
+        <h2 className="font-medium">Purpose of This Privacy Policy</h2>
+        <p>
+          This privacy policy aims to give you information on how Who Was At
+          collects and processes your personal data through your use of this
+          website, including any data you may provide through this site when you
+          sign up for our service.
+        </p>
+        <h2 className="font-medium">Controller</h2>
+        <p>
+          Who Was At is the controller and responsible for your personal data
+          (collectively referred to as "Company", "we", "us", or "our" in this
+          privacy policy).
+        </p>
+        <h2 className="font-medium">Contact Details</h2>
+        <p>
+          Our full details are: Full Name of Legal Entity: Aster Financial
+          Corporation Email Address: team@withaster.com
+        </p>
+        <h2 className="font-medium">The Data We Collect About You</h2>
+        <p>
+          We may collect, use, store, and transfer ! different kinds of personal
+          data about you which we have grouped together as follows: Identity
+          Data includes first name, last name, username or similar identifier.
+          Contact Data includes email address and telephone numbers. Technical
+          Data includes internet protocol (IP) address, your login data, browser
+          type and version, time zone setting and location, browser plug-in
+          types and versions,operating system and platform, and other technology
+          on the devices you use to access this website. Usage Data includes
+          information about how you use our website, products, and services.
+          Marketing and Communications Data includes your preferences in
+          receiving marketing from us and our third parties and your
+          communication preferences.
+        </p>
+      </div>
+    </main>
+  );
+}
